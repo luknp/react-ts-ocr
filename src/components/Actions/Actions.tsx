5544f19e5b6143d12d757461635a3fd9f3ab793b
luknp <luk.npl@gmail.com>
2021-10-26 20:49:45 +0200

    feat: add tooltip over button

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -88,18 +88,21 @@ export default function Actions({ files, lastFileProgressProcent, appState, resu
 
       <div className='result-text'>
         <input className='result-input' type='text' placeholder='result' value={resultInput} onChange={handleOnChange} />
-        <svg
-          className={`h-5 w-5 mr-2 ${isCopied ? 'text-green-400' : 'text-gray-400'} `}
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          onClick={handleCopyToClipboard}
-        >
-          <rect x='9' y='9' width='13' height='13' rx='2' ry='2' /> <path d='M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1' />
-        </svg>
+        <div className='icon-tooltip'>
+          <svg
+            className={`h-5 w-5 mr-2 ${isCopied ? 'text-green-400' : 'text-gray-400'} `}
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            onClick={handleCopyToClipboard}
+          >
+            <rect x='9' y='9' width='13' height='13' rx='2' ry='2' /> <path d='M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1' />
+          </svg>
+          {isCopied && <span className='tooltip'>Copied!</span>}
+        </div>
       </div>
     </div>
   );
