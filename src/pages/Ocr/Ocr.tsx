0375bde76b8ab43f28c4c9a789172bbf45d2d793
luknp <luk.npl@gmail.com>
2021-10-27 23:05:39 +0200

    feat: add info if file has not been added

--- src/pages/Ocr/Ocr.tsx
+++ src/pages/Ocr/Ocr.tsx
@@ -66,8 +66,19 @@ function Ocr() {
       <h3 className='title'>Upload your images to OCR processing</h3>
       <div className='content'>
         <Dropzone addNewFiles={data => setFiles(data)} />
-        {files.length > 0 && (
+        {files.length > 0 ? (
           <Actions files={files} lastFileProgressProcent={progressProcent} appState={currentState} resultText={resultText} />
+        ) : (
+          <ul id='gallery' className='flex flex-1 flex-wrap mt-16'>
+            <li id='empty' className='h-full w-full text-center flex flex-col items-center justify-center items-center'>
+              <img
+                className='mx-auto w-32'
+                src='https://user-images.githubusercontent.com/507615/54591670-ac0a0180-4a65-11e9-846c-e55ffce0fe7b.png'
+                alt='no data'
+              />
+              <span className='text-small text-gray-500'>No imege selected</span>
+            </li>
+          </ul>
         )}
       </div>
     </div>
